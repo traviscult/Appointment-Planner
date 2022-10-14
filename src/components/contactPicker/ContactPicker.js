33bb2a10be538e3bec342418eb1bcb46f5ea7afb
traviscult <60018617+traviscult@users.noreply.github.com>
2022-10-13 21:50:42 -0500

    added functionality to contact picker

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = (props) => {
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    props.setContact(value);
+  };
   return (
-    ContactPicker
+    <select onChange={handleChange}>
+      <option value="">Please Select</option>
+      {props.contacts.map((e, index) => {
+        return (
+          <option value={e.name} key={index}>
+            {e.name}
+          </option>
+        );
+      })}
+    </select>
   );
 };
